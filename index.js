6ea2902cf57bc78aedd2436c0db6f1bf4136ae0c
caralocke <cara.m.locke@gmail.com>
2021-04-21 11:40:12 -0400

    Was able to successfully complete task 1 after reviewing the material available to me. Working on Task 2.

--- index.js
+++ index.js
@@ -39,13 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person(attrs) {
-    this.name = attrs.name;
-    this.age = attrs.name;
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
   }
  
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
  
-
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
   
   
   
@@ -64,8 +76,19 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  Car.prototype.fill = function(gallons){
+    for(let i = 0; i < 10; i++){
+      if(this.stomach.length < 10){
+        this.stomach.push(gallons)
+      }
+    }
   }
   
   
